b581f9d7f52a7dec610dea037aabe114a6134b26
andrey28121989 <anenashev89@gmail.com>
2023-02-24 21:51:30 +0400

    feat: solution

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ module.exports = function toReadable (number) {
   const tenths = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
   const dozens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
  
-
   let string = number.toString();
   const stringLength = string.length;
  
@@ -16,15 +15,43 @@ module.exports = function toReadable (number) {
   const firstRank = splittedString[stringLength - 1]; // единицы
   const secondRank = splittedString[stringLength - 2]; //десятки
 
-  if (stringLength === 2 && secondRank === '1') {
+  if (stringLength === 2 && secondRank === '1') { // проверка c 10 до 19
     const result = tenths[+firstRank];
+    return result;
+  }
+
+  if (stringLength === 2) {
+    const firstRankWord = units[+firstRank]; // слово в массиве единиц
+    const secondRankWord = dozens[+secondRank]; // слово в массиве десяток
+    const resultString = `${secondRankWord} ${firstRank === '0' ? '' : firstRankWord}`;
+
+    return resultString.trim();
+  }
+
+  const thirdRank = splittedString[stringLength - 3] // сотни
+
+  if (stringLength === 3 && firstRank === '0' && secondRank === '0') {
+    const result = units[+thirdRank] + ' hundred';
+
+    return result;
+  }
+
+  if (stringLength === 3 && secondRank === '1') {
+    const result = units[+thirdRank] + ' hundred ' + tenths[+firstRank];
+
+    return result;
+  }
+
+  if (stringLength === 3 && secondRank === '0') {
+    const result = units[+thirdRank] + ' hundred ' + units[+firstRank];
+
     return result;
   }
 
   const firstRankWord = units[+firstRank]; // слово в массиве единиц
   const secondRankWord = dozens[+secondRank]; // слово в массиве десяток
-  const resultString = `${secondRankWord} ${firstRank === '0' ? '' : firstRankWord}`;
+  const thirdRankWord = units[+thirdRank] + ' hundred'; // первое слово для трехзначного числа
+  const resultString = `${thirdRankWord} ${secondRankWord} ${firstRank === '0' ? '' : firstRankWord}`;
 
   return resultString.trim();
-
 }
